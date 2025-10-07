db9a934b57fbf33f7fda4d8b7c49401ae5a92bdd
20062573 <20062573@mydbs.ie>
2025-10-07 13:33:48 +0000

    Update test4/index.js with [ change3]

--- test4/index.js
+++ test4/index.js
@@ -2,15 +2,15 @@ function processInput(input) {
   let parts = input.split(" : ");
   let factors = parts[0].split(" ").map(Number);
   let multiples = parts[1].split(" ");
-
   let sumFactors = 0;
   for (let i = 0; i < factors.length; i++) {
     sumFactors += factors[i];
   }
-
   let output = sumFactors + " : " + factors.join(" ") + " : " + multiples.join(" ");
   return output;
 }
-
-let input = "3 5 : 1 2 3 4 5 6 7 8 9";
-console.log(processInput(input));
+document.getElementById("processBtn").addEventListener("click", function() {
+  let input = document.getElementById("inputText").value;
+  let result = processInput(input);
+  document.getElementById("output").textContent = result;
+});
