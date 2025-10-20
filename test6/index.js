6a3248e5dc13f99e6b22ed98932852ae28972be6
20062573 <20062573@mydbs.ie>
2025-10-20 09:32:02 +0100

    chnage2

--- test6/index.js
+++ test6/index.js
@@ -2,5 +2,5 @@ const msg = document.querySelector('#message');
 const btn = document.querySelector('#changeBtn');
 
 btn.addEventListener('click', () => {
-  msg.textContent = "button is used";
+  msg.textContent = "button";
 });
